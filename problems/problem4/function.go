2b5b9b408cb19960a4e68d2a7b2857fd09c0cb1b
danikarik <baitursynov92@gmail.com>
2019-02-04 11:21:09 +0600

    update solution

--- problems/problem4/function.go
+++ problems/problem4/function.go
@@ -4,7 +4,8 @@ import (
 	"sort"
 )
 
-func solver(in []int) int {
+// O(n log n)
+func solverSort(in []int) int {
 	sort.Sort(sort.IntSlice(in))
 	next := 1
 	for _, n := range in {
@@ -14,4 +15,27 @@ func solver(in []int) int {
 		}
 	}
 	return next
+}
+
+// O(n)
+func solver(nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return 1
+	}
+	for i := 0; i < len(nums); i++ {
+		for i+1 != nums[i] && nums[i] > 0 && nums[i] <= len(nums) {
+			v := nums[i]
+			nums[i] = nums[v-1]
+			nums[v-1] = v
+			if nums[i] == nums[v-1] {
+				break
+			}
+		}
+	}
+	for i, num := range nums {
+		if num != i+1 {
+			return i + 1
+		}
+	}
+	return len(nums) + 1
 }
--- problems/problem4/function_test.go
+++ problems/problem4/function_test.go
@@ -2,40 +2,41 @@ package problem4
 
 import "testing"
 
+var testCases = []struct {
+	Given    []int
+	Expected int
+}{
+	{
+		Given:    []int{3, 4, -1, 1},
+		Expected: 2,
+	},
+	{
+		Given:    []int{1, 2, 0},
+		Expected: 3,
+	},
+	{
+		Given:    []int{-1, 0, 2},
+		Expected: 1,
+	},
+	{
+		Given:    []int{-4, -2, -1},
+		Expected: 1,
+	},
+	{
+		Given:    []int{1, 2, 5},
+		Expected: 3,
+	},
+	{
+		Given:    []int{-1, 0, 3},
+		Expected: 1,
+	},
+	{
+		Given:    []int{2, -1, 1, 4},
+		Expected: 3,
+	},
+}
+
 func TestSolver(t *testing.T) {
-	testCases := []struct {
-		Given    []int
-		Expected int
-	}{
-		{
-			Given:    []int{3, 4, -1, 1},
-			Expected: 2,
-		},
-		{
-			Given:    []int{1, 2, 0},
-			Expected: 3,
-		},
-		{
-			Given:    []int{-1, 0, 2},
-			Expected: 1,
-		},
-		{
-			Given:    []int{-4, -2, -1},
-			Expected: 1,
-		},
-		{
-			Given:    []int{1, 2, 5},
-			Expected: 3,
-		},
-		{
-			Given:    []int{-1, 0, 3},
-			Expected: 1,
-		},
-		{
-			Given:    []int{2, -1, 1, 4},
-			Expected: 3,
-		},
-	}
 	for _, c := range testCases {
 		res := solver(c.Given)
 		if res != c.Expected {
@@ -44,7 +45,19 @@ func TestSolver(t *testing.T) {
 	}
 }
 
-var result int
+func TestSolverSort(t *testing.T) {
+	for _, c := range testCases {
+		res := solverSort(c.Given)
+		if res != c.Expected {
+			t.Errorf("failed: got %v, expected %v, values %v", res, c.Expected, c.Given)
+		}
+	}
+}
+
+var (
+	result     int
+	resultSort int
+)
 
 func BenchmarkSolver(b *testing.B) {
 	var r int
@@ -53,4 +66,12 @@ func BenchmarkSolver(b *testing.B) {
 		b.Log(r)
 	}
 	result = r
+}
+func BenchmarkSolverSort(b *testing.B) {
+	var r int
+	for i := 0; i < b.N; i++ {
+		r = solverSort([]int{1, 2, 3, 4, 5})
+		b.Log(r)
+	}
+	resultSort = r
 }
