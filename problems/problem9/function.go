1eae912c812cc31ddcdf3e35764cc40b8e0d9cc7
danikarik <baitursynov92@gmail.com>
2019-02-07 00:56:36 +0600

    solve problem#9

---
+++ problems/problem9/function.go
@@ -0,0 +1,22 @@
+package problem9
+
+func solver(nums []int) int {
+	if nums == nil || len(nums) == 0 {
+		return 0
+	}
+	incl := nums[0]
+	excl, temp := 0, 0
+	for i := 1; i < len(nums); i++ {
+		temp = max(incl, excl)
+		incl = excl + nums[i]
+		excl = temp
+	}
+	return max(incl, excl)
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
---
+++ problems/problem9/function_test.go
@@ -0,0 +1,35 @@
+package problem9
+
+import "testing"
+
+func TestSolver(t *testing.T) {
+	testCases := []struct {
+		Given    []int
+		Expected int
+	}{
+		{
+			Given:    []int{2, 4, 6, 2, 5},
+			Expected: 13,
+		},
+		{
+			Given:    []int{5, 1, 1, 5},
+			Expected: 10,
+		},
+	}
+	for _, c := range testCases {
+		res := solver(c.Given)
+		if res != c.Expected {
+			t.Errorf("failed: got %v, expected %v, values %v", res, c.Expected, c.Given)
+		}
+	}
+}
+
+var result int
+
+func BenchmarkSolver(b *testing.B) {
+	var r int
+	for i := 0; i < b.N; i++ {
+		r = solver([]int{2, 4, 6, 2, 5})
+	}
+	result = r
+}
