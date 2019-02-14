523caec4ba8bd2572efded921d0b0963fe062cdd
danikarik <baitursynov92@gmail.com>
2019-02-14 17:21:31 +0600

    solve problem#15

--- problems/problem15/function.go
+++ problems/problem15/function.go
@@ -1,1 +1,25 @@
 package problem15
+
+import (
+	"math/rand"
+	"time"
+)
+
+func random(min, max int) int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return rand.Intn(max-min) + min
+}
+
+func pick(stream []int) int {
+	var element int
+	for i, e := range stream {
+		if i == 0 {
+			element = e
+		} else {
+			if random(1, i+1) == 1 {
+				element = e
+			}
+		}
+	}
+	return element
+}
--- problems/problem15/function_test.go
+++ problems/problem15/function_test.go
@@ -1,1 +1,27 @@
 package problem15
+
+import "testing"
+
+func TestPick(t *testing.T) {
+	testCases := []struct {
+		Given []int
+	}{
+		{
+			Given: []int{1, 2, 3, 4},
+		},
+	}
+	for _, c := range testCases {
+		res := pick(c.Given)
+		t.Logf("res: %v\n", res)
+	}
+}
+
+var result int
+
+func BenchmarkPick(b *testing.B) {
+	var r int
+	for i := 0; i < b.N; i++ {
+		r = pick([]int{1, 2, 3, 4})
+	}
+	result = r
+}
